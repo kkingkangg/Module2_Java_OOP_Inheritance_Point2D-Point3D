fa21d38120af786103c7bcfe68b61d15d249f48a
kkingkangg <phungnghia152@gmail.com>
2020-09-19 16:30:35 +0700

    Bài tập kế thừa :point2D và point3D

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(3,6);
+        Point2D point3D = new Point3D(5,3,6);
+
+        System.out.println(point2D);
+        System.out.println(point3D);
+    }
+}
